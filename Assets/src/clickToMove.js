41aa96a560fdf1859c9caebc4ab81a6a7a34156f
Michael Hoyle <hoylemd@gmail.com>
2014-02-23 13:36:11 -0500

    moved the player on rght click
    resolves #2

--- Assets/src/clickToMove.js
+++ Assets/src/clickToMove.js
@@ -1,34 +1,30 @@
 ﻿#pragma strict
 
-public var explosion: GameObject;
-
 // fuction to easily get mouse position in game space coordinates
 function getMouseWorldPosition() : Vector3 {
 	var position = Input.mousePosition;
-	
+
 	// adjust by camera's z position
 	position.z = Camera.main.transform.localPosition.z * -1;
-	
+
 	return Camera.main.ScreenToWorldPoint(position);
 }
 
 function Update () {
 	var position: Vector3;
 	var new_explosion: GameObject;
-	
+
 	// handle right clicks
 	if (Input.GetMouseButtonDown(0)) {
 	}
-	
+
 	// handle right clicks
 	if (Input.GetMouseButtonDown(1)) {
 		// get position
 		position = getMouseWorldPosition();
-		
-		if (explosion) {
-			new_explosion = Instantiate(explosion);
-			new_explosion.transform.position = position;
-		}
+
+        // move the parent
+        transform.position = position;
 	}
-	
+
 }
