4e7902d107ac803d058fb06c7a40bafaa761a84a
Michael Hoyle <hoylemd@gmail.com>
2014-02-23 12:37:02 -0500

    Added click handler and explosion
    Made an update script that detects right and left clicks. Also added a simple explosion sprite to use as a spot indicator.

---
+++ Assets/src/clickToMove.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+public var explosion:GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetMouseButtonDown(1)) {
+		Debug.Log("click");
+		if (explosion) {
+			Instantiate(explosion);
+		}
+	}
+	
+	if (Input.GetMouseButtonDown(0)) {
+		Debug.Log("click1");
+	}
+}
